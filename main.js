bfd7514cd2309ca0d56a4cd766064ccd7f23d2d8
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2023-08-13 16:49:54 -0300

    updade

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ function comprar(){
         img.style.display = 'block'
         bts.style.display = 'block'
         res.innerHTML = arroz
-       
+        
        
     }else if (nomeProduto==2){
         img2.style.display = 'block'
         res.innerHTML = feijao
         bts.style.display = 'block'
+       
     }
     else if (nomeProduto==3){
         res.innerHTML = macarrao
@@ -23,4 +24,24 @@ function comprar(){
         res.innerHTML = "Produto não cadastrado"
     }
    
+}
+function pagar(){
+    opcoesPagamento.style.display = 'block'
+    
+}
+function voltar(){
+    bts.style.display = 'none'
+    opcoesPagamento.style.display = 'none'
+    pix1.style.display = 'none'
+    boleto1.style.display = 'none'
+    cartao1.style.display = 'none'
+}
+function pix(){
+    pix1.style.display = 'block';
+}
+function boleto(){
+    boleto1.style.display = 'block'
+}
+function cartao(){
+    cartao1.style.display = 'block'
 }
